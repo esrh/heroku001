297fb13679fdf92fbdc67f61e2342cf02d6220f8
esrh <esrh@users.noreply.github.com>
2018-06-15 00:47:41 +0900

    Automatically update

--- main.py
+++ main.py
@@ -9,6 +9,9 @@ from linebot.models import (
     MessageEvent, TextMessage, TextSendMessage,
 )
 import os
+import socket
+import binascii
+import json
 
 app = Flask(__name__)
 YOUR_CHANNEL_ACCESS_TOKEN = os.environ["YOUR_CHANNEL_ACCESS_TOKEN"]
@@ -29,7 +32,7 @@ def callback():
 
 @app.route("/iwana")
 def iwana():
-    return 'hello'
+    return iwana_now()
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
@@ -37,6 +40,19 @@ def handle_message(event):
         event.reply_token,
         TextSendMessage(text=event.message.text))
 
+def iwana_now():
+    data = "1900d402123138377365727665722e78797a00464d4c0063dd01010009010000000006452306"
+    data = binascii.unhexlify(data)
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client.connect(("187server.xyz" ,25565))
+    client.send(data)
+    response = client.recv(1024)
+    ans = binascii.hexlify(response)[10:]
+    ans = binascii.unhexlify(ans).decode('utf-8', 'replace') + '"}'
+    ans = json.loads(ans)["players"]["online"]
+    client.close()
+    return ans
+    
 if __name__ == "__main__":
 #    app.run()
     port = int(os.getenv("PORT", 5000))

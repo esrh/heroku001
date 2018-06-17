67d62e768060086091c50b58429cd8e474cde116
esrh <esrh@users.noreply.github.com>
2018-06-17 16:53:28 +0900

    Automatically update

---
+++ iwana/iwana.py
@@ -0,0 +1,20 @@
+def iwana():
+    x = {}
+    try:
+        x["iwana_now"] = iwana_now()
+    except ConnectionRefusedError:
+        x["error"] = '187 refused!'
+    return json.dumps(x)
+    
+def iwana_now():
+    data = "1900d402123138377365727665722e78797a00464d4c0063dd01010009010000000006452306"
+    data = binascii.unhexlify(data)
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client.connect(("187server.xyz" ,25565))
+    client.send(data)
+    response = client.recv(1024)
+    ans = binascii.hexlify(response)[10:]
+    ans = binascii.unhexlify(ans).decode('utf-8', 'replace') + '"}'
+    ans = json.loads(ans)["players"]["online"]
+    client.close()
+    return ans
--- main.py
+++ main.py
@@ -13,6 +13,8 @@ import socket
 import binascii
 import json
 
+import iwana
+
 app = Flask(__name__)
 YOUR_CHANNEL_ACCESS_TOKEN = os.environ["YOUR_CHANNEL_ACCESS_TOKEN"]
 YOUR_CHANNEL_SECRET = os.environ["YOUR_CHANNEL_SECRET"]
@@ -32,12 +34,7 @@ def callback():
 
 @app.route("/iwana")
 def iwana():
-    x = {}
-    try:
-        x["iwana_now"] = iwana_now()
-    except ConnectionRefusedError:
-        x["error"] = '187 refused!'
-    return json.dumps(x)
+    iwana.iwana()
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
@@ -50,18 +47,7 @@ def handle_message(event):
             TextSendMessage(text=event.message.text)
         )
 
-def iwana_now():
-    data = "1900d402123138377365727665722e78797a00464d4c0063dd01010009010000000006452306"
-    data = binascii.unhexlify(data)
-    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client.connect(("187server.xyz" ,25565))
-    client.send(data)
-    response = client.recv(1024)
-    ans = binascii.hexlify(response)[10:]
-    ans = binascii.unhexlify(ans).decode('utf-8', 'replace') + '"}'
-    ans = json.loads(ans)["players"]["online"]
-    client.close()
-    return ans
+
     
 if __name__ == "__main__":
 #    app.run()

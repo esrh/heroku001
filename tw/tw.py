a5535998840a8a6a1659db435979ff715c2aa038
esrh <esrh@users.noreply.github.com>
2018-06-22 00:01:48 +0900

    Automatically update

--- tw/tw.py
+++ tw/tw.py
@@ -18,7 +18,7 @@ def __keys():
         url = 'http://localhost:5000/tw/callback'
     return key, s, url
 
-@app.route('/tw')
+@app.route('/tw/')
 def main():
     if "oauth_token" not in session:
         data = 'you need to login'
@@ -27,7 +27,7 @@ def main():
     return render_template('tw.html', data=data, auth_url='tw/login', title='collectmachine')
 
 
-@app.route('/tw/login')
+@app.route('/tw/login/')
 def login_redirect():
     key, secret, url = __keys()
     return redirect(tw_auth.get_request_token(key, secret, url))
@@ -40,7 +40,7 @@ def tw_callback():
     session['oauth_token'], session['oauth_token_secret'] = dc
     return redirect('/tw') 
 
-@app.route('/tw/collect')
+@app.route('/tw/collect/')
 def collect_tweet_400():
     consumer_key, consumer_secret, hoge = __keys()
     if "oauth_token" not in session:

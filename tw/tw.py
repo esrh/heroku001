2489cb45c4fc79799ccc632a6c3b4ae1dcc699e9
esrh <esrh@users.noreply.github.com>
2018-06-21 23:54:28 +0900

    Automatically update

--- tw/tw.py
+++ tw/tw.py
@@ -1,8 +1,9 @@
 from flask import (Blueprint, render_template, request, redirect,
   session, make_response, send_file,
   make_response, send_from_directory)
-import os
+import os, io
 from tw import tw_auth
+from twitter import *
 
 app = Blueprint('tw', __name__)
 
@@ -38,3 +39,34 @@ def tw_callback():
     dc = tw_auth.get_access_token(a, b, c, d)
     session['oauth_token'], session['oauth_token_secret'] = dc
     return redirect('/tw') 
+
+@app.route('/tw/collect')
+def collect_tweet_200():
+    consumer_key, consumer_secret, hoge = __keys()
+    if "oauth_token" not in session:
+        return 'need login'
+    a, b = session.get('oauth_token', ''), session.get('oauth_token_secret', '')
+    t = Twitter(auth=OAuth(a, b, consumer_key, consumer_secret))
+    user_id = session.get('oauth_token', '').split('-')[0]
+    xc = '"tweet","created_at","source"\n'
+    x = None
+    for count in range(2):
+        if x is not None:
+            x = t.statuses.user_timeline(count=200, user_id=user_id, max_id=x[-1]['id'] - 1)
+        else:
+            x = t.statuses.user_timeline(count=200, user_id=user_id)
+        for xx in x:
+            xc += '"{}"'.format(xx['text'].replace('"', '""'))
+            xc += ',"{}"\n'.format(xx['created_at'].replace('"', '""'))
+            xc += ',"{}"\n'.format(xx['source'].replace('"', '""'))
+        if x == '':
+            break
+    bi = io.BytesIO()
+    bi.write(xc.encode('utf-8'))
+    res = make_response()
+    res.data = bi.getvalue()
+    res.headers['Content-Type'] = 'text/plain'
+    res.headers['Content-Disposition'] = 'attachment; filename=data.csv'
+    bi.close()
+    return res
+
